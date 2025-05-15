607e9421a84d7624ae981727aed33c37037c4dab
Derick Walker <dcwalker@usc.edu>
2025-05-15 14:56:43 -0700

    Create stack

---
+++ backend/src/main/java/com/locationchecker/LocationCheckerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.locationchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationCheckerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationCheckerBackendApplication.class, args);
+	}
+
+}
